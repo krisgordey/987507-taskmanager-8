26eb7123b283068c7c19eba38509f998701dc69b
Kristina Gordeeva <krisgordeeva@gmail.com>
2019-02-23 15:54:49 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    path: path.join(__dirname, `public/js`)
   },
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
     hot: true,
-    compress: true
+    compress: false
   }
 };
